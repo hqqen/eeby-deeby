e1f647d1a2af5b017b24c2f3c89ba12ffa972c70
notbeyer <108302678+notAlex0@users.noreply.github.com>
2022-12-09 00:17:57 -0500

    working acrobot nn commit
    need to dial in parameters to get model to converge

--- class-work/IntroML/project/nn-rework/nn.py
+++ class-work/IntroML/project/nn-rework/nn.py
@@ -17,25 +17,30 @@ from torch.distributions import Categorical
 
 #build learning policy
 class acroNN(nn.Module):
-    def __init__(self, nIn = 6, nHide = 32, nOut = 3):
+    def __init__(self, nIn = 6, nHide = 32, nOut = 3):#6, 32, 3
         super(acroNN, self).__init__()
         self.fc1 = nn.Linear(nIn, nHide)
+        self.fc1a = nn.Linear(nHide, nHide)
         self.fc2 = nn.Linear(nHide,nOut)
 
     def forward(self, state):
-        state = func.relu(self.fc1(state))
+        state = func.leaky_relu(self.fc1(state))
+        state = func.leaky_relu(self.fc1a(state))
         state = self.fc2(state)
         return func.softmax(state, dim = 1)
 
     def act(self, state):
-        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        if type(state) == tuple:
+            state = torch.from_numpy(state[0]).float().unsqueeze(0).to(device)
+        else:
+            state = torch.from_numpy(state).float().unsqueeze(0).to(device)
         probs = self.forward(state).cpu()
         m = Categorical(probs)
         nextAction = m.sample()
         return nextAction.item() - 1, m.log_prob(nextAction)
 
 #define reinforcement learning policy
-def reinforce(network, nEps = 5000, tMax = 100, gamma = 1.0, nPrint = 100):
+def reinforce(network, nEps = 100, tMax = 1000, gamma = .05, nPrint = 100): #set tiem back to 100????
     scoresDEQ = deque(maxlen = 100)
     score = []
     for ep in range(1,nEps+1):
@@ -52,14 +57,18 @@ def reinforce(network, nEps = 5000, tMax = 100, gamma = 1.0, nPrint = 100):
         scoresDEQ.append(sum(rewards))
         score.append(sum(rewards))
         discounts = [gamma**i for i in range(len(rewards) + 1)]
-        R = sum([(disc, reward) for (disc, reward) in zip(discounts,rewards)])
+        #R = sum([(disc, reward) for (disc, reward) in zip(discounts,rewards)])
+        R = 0
+        for i in range(np.amin([np.size(discounts), np.size(rewards)])):
+            R += discounts[i]*rewards[i]
         loss = []
         for logProb in logProbs:
             loss.append(-logProb*R)
-        loss = torch.cat(loss).sum()
+        lossSum = torch.cat(loss).sum()
         optimizer.zero_grad()
-        loss.backwards()
+        lossSum.backward()
         optimizer.step()
+    return loss, lossSum, score
 
 
 #init environment - seed rng for consistency across runs 
@@ -67,5 +76,7 @@ torch.manual_seed(0)
 device = torch.device("cpu") #replace with "cuda" if running on cuda capable hardware
 env = gym.make('Acrobot-v1')
 network = acroNN()
-optimizer = optim.Adam(network.parameters(), lr=0.001)
-reinforce(network)
+optimizer = optim.Adam(network.parameters(), lr=0.005)
+loss, lossSum, score = reinforce(network)
+
+1 == 1 
