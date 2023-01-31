5ef48821a154fca54c6cb5afb88d36255c9cffca
notAlex0 <abeyer@umd.edu>
2023-01-31 00:49:51 -0500

    working ipg locn commit
    diverges but runs

--- Distributed Robotic Beamforming/Alex Code/Two Level Optimizer/position optimzier/ipgPosition.m
+++ Distributed Robotic Beamforming/Alex Code/Two Level Optimizer/position optimzier/ipgPosition.m
@@ -24,8 +24,8 @@ Na = length(r);     % num agents
 Ns = length(rho);   % num points to sample over
 gamma = ones(Na,Ns);% placeholder no noise gamma
 mu = 2;             % path loss exponent
-a = zeros(Na,T); a(:,1) = reshape(a0,[Na,1]);
-alpha = zeros(Na,T); alpha(:,1) = reshape(alpha0,Na,1);
+a = ones(Na,T).*reshape(a0,[Na,1]);
+alpha = ones(Na,T).*reshape(alpha0,[Na,1]);
 r = reshape(r,2,max(size(r)));
 x = r(1,:).';         % split agent posn vector r into x and y posns
 xOld = zeros(Na,T);
@@ -62,12 +62,12 @@ for t = 1:T
     dxy = zeros(Na,Ns);
     Lx = zeros(Na,Ns);
     Ly = zeros(Na,Ns);
-    Lxx = zeros(Na,Ns);
-    Lyy = zeros(Na,Ns);
-    Lxy = zeros(Na,Ns);
+    Lxx = zeros(Na);
+    Lyy = zeros(Na);
+    Lxy = zeros(Na);
     exponent = zeros(Na,Ns);
-    gx = zeros(Na,Ns);
-    gy = zeros(Na,Ns);
+%     gx = zeros(Na,Ns);
+%     gy = zeros(Na,Ns);
     h = zeros(2*Na);
 
     % update the noiseless channel parameters to send to server
@@ -85,52 +85,79 @@ for t = 1:T
         for agent = 1:Na
             den = den + a(agent,t)*(u(agent,rec) + 1i*v(agent,rec));
             % derivative of Tx-Rx distance
-            exponent(agent,rec) = exp(j*(alpha(agent,t)* + k*(x(agent,:)*cos(tht(rec,:)) + y(agent,:)*sin(tht(rec,:)) + d(agent,rec))));
+            exponent(agent,rec) = exp(1j*(alpha(agent,t)* + k*(x(agent,:)*cos(tht(rec,:)) + y(agent,:)*sin(tht(rec,:)) + d(agent,rec))));
             dx(agent,rec) = (x(agent,:)-rho(rec,:)*cos(tht(rec,:)))*1/sqrt((x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2 + (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2);
             dy(agent,rec) = (y(agent,:)-rho(rec,:)*sin(tht(rec,:)))*1/sqrt((x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2 + (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2);
             dxx(agent,rec) = (1/sqrt((x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2 + (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2)) - (x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2*((x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2 + (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2)^(-3/2);
             dyy(agent,rec) = (1/sqrt((x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2 + (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2)) - (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2*((x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2 + (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2)^(-3/2);
             dxy(agent,rec) = (x(agent,:) - rho(rec,:)*cos(tht(rec,:)))*(y(agent,:) - rho(rec,:)*sin(tht(rec,:)))*((x(agent,:) - rho(rec,:)*cos(tht(rec,:)))^2 + (y(agent,:) - rho(rec,:)*sin(tht(rec,:)))^2)^(-3/2);
             % gradient of cost function
+            % should be of dim'ality Na*Ns, summed along the dir of size Ns
+            % to get gradient at each agent
             Lx(agent,rec) = (w(rec,:)/2)*((a(agent,t)*gamma(agent,rec)/d(agent,rec))*(k*(cos(tht(rec,:)) + dx(agent,rec))) - a(agent,t)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec);
             Ly(agent,rec) = (w(rec,:)/2)*((a(agent,t)*gamma(agent,rec)/d(agent,rec))*(k*(sin(tht(rec,:)) + dy(agent,rec))) - a(agent,t)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec);
             % hessian of cost function
-            Lxx(agent,rec) = w(rec,:)*(-(a(agent,t)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec)^(-2))*(k*(cos(tht(rec,:)) + dx(agent,rec)))*exponent(agent,rec) ...
-                             +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*k*dxx(agent,rec))*exponent(agent,rec) ...
-                             +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*(k*(cos(tht(rec,:))+dx(agent,rec)))^2)*exponent(agent,rec) ...
-                             -(a(agent,t)*gamma(agent,rec)*dxx(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec) ...
-                             +2*(a(agent,t)*gamma(agent,rec)*dx(agent,rec)^2*d(agent,rec)^(-3))*exponent(agent,rec) ...
-                             -(a(agent,rec)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec)^(-2))*k*(cos(tht(rec,:)) + dx(agent,rec))*exponent(agent,rec));
-            Lyy(agent,rec) = w(rec,:)*(-(a(agent,t)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec)^(-2))*(k*(sin(tht(rec,:)) + dy(agent,rec)))*exponent(agent,rec) ...
-                             +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*k*dyy(agent,rec))*exponent(agent,rec) ...
-                             +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*(k*(sin(tht(rec,:))+dy(agent,rec)))^2)*exponent(agent,rec) ...
-                             -(a(agent,t)*gamma(agent,rec)*dyy(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec) ...
-                             +2*(a(agent,t)*gamma(agent,rec)*dy(agent,rec)^2*d(agent,rec)^(-3))*exponent(agent,rec) ...
-                             -(a(agent,rec)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec)^(-2))*k*(sin(tht(rec,:)) + dy(agent,rec))*exponent(agent,rec)); 
-            Lxy(agent,rec) = w(rec,:)*(-(a(agent,t)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec)^(-2))*(k*(sin(tht(rec,:)) + dy(agent,rec)))*exponent(agent,rec) ...
-                             +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*k*dxy(agent,rec))*exponent(agent,rec) ...
-                             +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*(k*(sin(tht(rec,:))+dy(agent,rec)))*(k*(cos(tht(rec,:))+dx(agent,rec)))^2)*exponent(agent,rec) ...
-                             -(a(agent,t)*gamma(agent,rec)*dxy(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec) ...
-                             +2*(a(agent,t)*gamma(agent,rec)*dx(agent,rec)*dy(agent,rec)*d(agent,rec)^(-3))*exponent(agent,rec) ...
-                             -(a(agent,rec)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec)^(-2))*k*(cos(tht(rec,:))+dx(agent,rec))*exponent(agent,rec)); 
+            % should be of size Na*Na, used in multiplication - will need
+            % to consist of two nested loops through each agent summed
+            % across all Rxs
+            % each of these 2nd derivative matricies should be of dim'ality
+            % Na*Na, theyll stack into the full thing
+            %             Lxx(agent,rec) = w(rec,:)*(-(a(agent,t)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec)^(-2))*(k*(cos(tht(rec,:)) + dx(agent,rec)))*exponent(agent,rec) ...
+            %                 +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*k*dxx(agent,rec))*exponent(agent,rec) ...
+            %                 +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*(k*(cos(tht(rec,:))+dx(agent,rec)))^2)*exponent(agent,rec) ...
+            %                 -(a(agent,t)*gamma(agent,rec)*dxx(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec) ...
+            %                 +2*(a(agent,t)*gamma(agent,rec)*dx(agent,rec)^2*d(agent,rec)^(-3))*exponent(agent,rec) ...
+            %                 -(a(agent,rec)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec)^(-2))*k*(cos(tht(rec,:)) + dx(agent,rec))*exponent(agent,rec));
+            %             Lyy(agent,rec) = w(rec,:)*(-(a(agent,t)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec)^(-2))*(k*(sin(tht(rec,:)) + dy(agent,rec)))*exponent(agent,rec) ...
+            %                 +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*k*dyy(agent,rec))*exponent(agent,rec) ...
+            %                 +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*(k*(sin(tht(rec,:))+dy(agent,rec)))^2)*exponent(agent,rec) ...
+            %                 -(a(agent,t)*gamma(agent,rec)*dyy(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec) ...
+            %                 +2*(a(agent,t)*gamma(agent,rec)*dy(agent,rec)^2*d(agent,rec)^(-3))*exponent(agent,rec) ...
+            %                 -(a(agent,rec)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec)^(-2))*k*(sin(tht(rec,:)) + dy(agent,rec))*exponent(agent,rec));
+            %             Lxy(agent,rec) = w(rec,:)*(-(a(agent,t)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec)^(-2))*(k*(sin(tht(rec,:)) + dy(agent,rec)))*exponent(agent,rec) ...
+            %                 +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*k*dxy(agent,rec))*exponent(agent,rec) ...
+            %                 +(a(agent,t)*gamma(agent,rec)*d(agent,rec)^(-1)*(k*(sin(tht(rec,:))+dy(agent,rec)))*(k*(cos(tht(rec,:))+dx(agent,rec)))^2)*exponent(agent,rec) ...
+            %                 -(a(agent,t)*gamma(agent,rec)*dxy(agent,rec)*d(agent,rec)^(-2))*exponent(agent,rec) ...
+            %                 +2*(a(agent,t)*gamma(agent,rec)*dx(agent,rec)*dy(agent,rec)*d(agent,rec)^(-3))*exponent(agent,rec) ...
+            %                 -(a(agent,rec)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec)^(-2))*k*(cos(tht(rec,:))+dx(agent,rec))*exponent(agent,rec));
+            Lxx(1:Na,agent) = Lxx(1:Na,agent) + w(rec,:)*(-(a(:,t)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec).^(-2))*(k*(cos(tht(rec,:)) + dx(agent,rec)))*exponent(agent,rec) ...
+                +(a(:,t)*gamma(agent,rec)*d(agent,rec).^(-1)*k*dxx(agent,rec))*exponent(agent,rec) ...
+                +(a(:,t)*gamma(agent,rec)*d(agent,rec).^(-1)*(k*(cos(tht(rec,:))+dx(agent,rec))).^2)*exponent(agent,rec) ...
+                -(a(:,t)*gamma(agent,rec)*dxx(agent,rec)*d(agent,rec).^(-2))*exponent(agent,rec) ...
+                +2*(a(:,t)*gamma(agent,rec)*dx(agent,rec).^2*d(agent,rec).^(-3))*exponent(agent,rec) ...
+                -(a(:,rec)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec).^(-2))*k*(cos(tht(rec,:)) + dx(agent,rec))*exponent(agent,rec));
+            Lyy(1:Na,agent) = Lyy(1:Na,agent) + w(rec,:)*(-(a(:,t)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec).^(-2))*(k*(sin(tht(rec,:)) + dy(agent,rec)))*exponent(agent,rec) ...
+                +(a(:,t)*gamma(agent,rec)*d(agent,rec).^(-1)*k*dyy(agent,rec))*exponent(agent,rec) ...
+                +(a(:,t)*gamma(agent,rec)*d(agent,rec).^(-1)*(k*(sin(tht(rec,:))+dy(agent,rec))).^2)*exponent(agent,rec) ...
+                -(a(:,t)*gamma(agent,rec)*dyy(agent,rec)*d(agent,rec).^(-2))*exponent(agent,rec) ...
+                +2*(a(:,t)*gamma(agent,rec)*dy(agent,rec).^2*d(agent,rec).^(-3))*exponent(agent,rec) ...
+                -(a(:,rec)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec).^(-2))*k*(sin(tht(rec,:)) + dy(agent,rec))*exponent(agent,rec));
+            Lxy(1:Na,agent) = Lxy(1:Na,agent) + w(rec,:)*(-(a(:,t)*gamma(agent,rec)*dx(agent,rec)*d(agent,rec).^(-2))*(k*(sin(tht(rec,:)) + dy(agent,rec)))*exponent(agent,rec) ...
+                +(a(:,t)*gamma(agent,rec)*d(agent,rec).^(-1)*k*dxy(agent,rec))*exponent(agent,rec) ...
+                +(a(:,t)*gamma(agent,rec)*d(agent,rec).^(-1)*(k*(sin(tht(rec,:))+dy(agent,rec)))*(k*(cos(tht(rec,:))+dx(agent,rec))).^2)*exponent(agent,rec) ...
+                -(a(:,t)*gamma(agent,rec)*dxy(agent,rec)*d(agent,rec).^(-2))*exponent(agent,rec) ...
+                +2*(a(:,t)*gamma(agent,rec)*dx(agent,rec)*dy(agent,rec)*d(agent,rec).^(-3))*exponent(agent,rec) ...
+                -(a(:,rec)*gamma(agent,rec)*dy(agent,rec)*d(agent,rec).^(-2))*k*(cos(tht(rec,:))+dx(agent,rec))*exponent(agent,rec));
         end
         den = abs(den);
         AF(rec,t) = den;
+        
         % numerator is error (diff btwn des'd and rec'd AF)
         % now calculate the gradient at each reciever
 
         % gx(:,rec) = w(rec,:)*0;
         % gy(:,rec) = w(rec,:)*(num/den*(-(a(:,t)'*u(:,rec))*a(:,t).*v(:,rec) + (a(:,t)'*v(:,rec))*a(:,t).*u(:,rec)));
     end
+    Lx = abs(Lx); Ly = abs(Ly); Lxx = abs(Lxx); Lyy = abs(Lyy); Lxy = abs(Lxy);
     AFDB(:,t) = 20*log10(AF(:,t)/max(AF(:,t)));
     errorDB(t) = norm(AFDB(:,t) - 20*log10(f/max(f)),1)/Ns;
 
 
     %build hessian
     h(1:Na,1:Na) = Lxx;
-    h(Na:end,1:Na) = Lxy.';
-    h(1:Na,Na:end) = Lxy;
-    h(Na:end,Na:end) = Lyy;
+    h(Na+1:end,1:Na) = Lxy.';
+    h(1:Na,Na+1:end) = Lxy;
+    h(Na+1:end,Na+1:end) = Lyy;
     % for agent = 1:Na
     %     for rec = 1:Ns
     %         den = 0;
@@ -140,7 +167,7 @@ for t = 1:T
     %         end
     %         den = abs(den);
     %         num = den - f(rec,:);
-    % 
+    %
     %         % build hessian
     %         h(1:Na,agent) = h(1:Na,agent) + w(rec,:)*(num/den*(u(agent,rec)*u(:,rec)+v(agent,rec)*v(:,rec)) + f(rec,:)/den^3*(u(agent,rec)*u(:,rec)'*a(:,t)+v(agent,rec)*v(:,rec)'*a(:,t))*(u(:,rec)*u(:,rec)'*a(:,t)+v(:,rec)*v(:,rec)'*a(:,t)));
     %         h(1:Na,agent+Na) = h(1:Na,agent+Na) + w(rec,:)*(num/den*(-u(agent,rec)*a(:,t).*v(:,rec)-a(:,t)'*u(:,rec)*v(:,rec).*I(:,agent)+v(agent,rec)*a(:,t).*u(:,rec)+a(:,t)'*v(:,rec)*u(:,rec).*I(:,agent))...
@@ -156,10 +183,10 @@ for t = 1:T
     eps1 = 1/(max(eig(h)) + beta);
     % run GD update
     K = K - eps1*(h*K+beta*K-eye(2*Na));         % mass update preconditioner (using eqn 5, not 12)
-    grad(:,t) = [sum(ga,2);sum(galpha,2)];            % agent-wise gradient is summed
-    x = [a(:,t);alpha(:,t)] - eps2*K*grad(:,t);   % gradient update (eqn 4)
-    a(:,t+1) = x(1:Na,:);                            % split gradient update to amplitude and phase
-    alpha(:,t+1) = x(Na+1:2*Na,:);
+    grad(:,t) = [sum(Lx,2);sum(Ly,2)];            % agent-wise gradient is summed
+    g = [x(:);y(:)] - eps2*K*grad(:,t);   % gradient update (eqn 4)
+    x(:) = g(1:Na);                            % split gradient update to amplitude and phase
+    y(:) = g(Na+1:end);
     grad_norm(:,t) = norm(grad(:,t));                 % take l2 norm of grad
 
     % now calculate the true rec'd AF under the effect of noise
@@ -223,10 +250,18 @@ for t = 1:T
         % grid on;
         % set(gca, 'FontSize', 24)
 
+        figure(60606); hold off
+        scatter(x,y,'b','LineWidth',4); hold on
+        scatter(rho.*cos(tht),rho.*sin(tht),'k','LineWidth',4)
+        title("Agent Positions")
+        xlabel("x (m)"); ylabel("y (m)");
+%         exportgraphics(gcf,'agentMovementNoSBL.gif','Append',true);
+        grid on
+
 
     end
 end
-figure(60606)                                   % plot
+figure(6060606)                                   % plot
 plot(tht,noisyAFDB(:,1), 'g', 'LineWidth', 5); hold on
 plot(tht,noisyAFDB(:,T/10), 'r', 'LineWidth', 5)
 plot(tht,noisyAFDB(:,T/5), 'b', 'LineWidth', 5)
